c96cf31e020c9f9beeacc799e6da6dcdcdf8fe65
kimu-y7 <y.kimura@e-corporation.co.jp>
2026-04-16 13:36:53 +0900

    Add files via upload

--- kadai_mdn_js/kadai_mdn_js/mdn.js
+++ kadai_mdn_js/kadai_mdn_js/mdn.js
@@ -1,9 +1,7 @@
 const today = new Date();
 
-const year = today.getFullYear();   
-const month = today.getMonth() + 1; 
-const day = today.getDate();        
+const year = today.getFullYear();
+const month = today.getMonth() + 1;
+const day = today.getDate();
 
-const dateString = year + "年" + month + "月" + day + "日";
-
-console.log(dateString);
+console.log(`${year}年${month}月${day}日`);
