725ea6b9c23e0e26a2cffe488aaed492aaff7098
kimu-y7 <y.kimura@e-corporation.co.jp>
2026-04-16 14:10:18 +0900

    Add files via upload

---
+++ kadai_020/event.js
@@ -0,0 +1,7 @@
+const text = document.getElementById('text');
+
+const button1 = document.getElementById('btn');
+
+button1.addEventListener('click', function () {
+    text.textContent = 'ボタンをクリックしました';
+})
